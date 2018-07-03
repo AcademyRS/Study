f4337dee9ccb97609285043670ba85ba0f0691cb
lucasdefelippe <lucasjfelippo@gmail.com>
2018-07-03 02:09:08 -0300

    Lucas changes 75%

--- js/navbar.js
+++ js/navbar.js
@@ -1,13 +1,17 @@
 function opennavbar() {
     let navbar = document.getElementById('navbar');
+    let navbarclose = document.getElementById('navbar-close-div');
     if (navbar.className === 'navbar') {
         navbar.className += ' navbar-open';
+        navbarclose.className += ' navbar-close-div-open';
     }
 }
 
 function closenavbar() {
     let navbar = document.getElementById('navbar');
+    let navbarclose = document.getElementById('navbar-close-div');
     if (navbar.className === 'navbar navbar-open') {
         navbar.className = 'navbar';
+        navbarclose.className = 'navbar-close-div';
     }
 }
